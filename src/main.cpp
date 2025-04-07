4b67aabff0a7d50f70b9a16a9f7fba8c5440c064
DocMuerde <ialvarado@us.es>
2025-04-07 17:23:25 +0200

    Agrega impresión por serial al programa blik

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,14 @@
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
+  Serial.println("Inicio del parpadeo del LED");
 }
 
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
+  delay(1000);
   Serial.println("ON");
   digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
+  delay(1000);
   Serial.println("OFF");
 }
